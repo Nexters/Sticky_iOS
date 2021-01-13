26412dd9430e67226d96b2bed6f6337ee8c928bd
Kim Mincheol <nexters@kakao.com>
2021-01-13 23:33:08 +0900

    Initial Commit

---
+++ Sticky/StickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyApp.swift
+//  Sticky
+//
+//  Created by deo on 2021/01/13.
+//
+
+import SwiftUI
+
+@main
+struct StickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
