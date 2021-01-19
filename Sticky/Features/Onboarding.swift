9a7eb29ad987f90ff79ad31ea2ee4eedd2822d24
Kim Mincheol <mcauto@naver.com>
2021-01-19 23:54:00 +0900

    fix(on-boarding): 화면 디자인 변경 반영

--- Sticky/Features/Onboarding.swift
+++ Sticky/Features/Onboarding.swift
@@ -8,36 +8,41 @@
 import SwiftUI
 
 struct Onboarding: View {
+    init() {
+        let newNavAppearance = UINavigationBarAppearance()
+        newNavAppearance.configureWithTransparentBackground()
+        newNavAppearance.backgroundColor = .clear
+        UINavigationBar.appearance()
+            .standardAppearance = newNavAppearance
+    }
+
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Sticky")
-                    .font(Font.custom("Nunito", size: 24))
-                    .bold()
-                    .padding(.bottom, 32)
+            ZStack {
+                Color.main
+                    .ignoresSafeArea()
+                VStack {
+                    Image("logo_sticky")
+                        .padding(.bottom, 40)
+                    Text("“Sticky”는 집에서 사용하는 챌린지 서비스입니다. 집 위치를 설정하기 위해 위치정보 사용을 동의해주세요!".localized)
+                        .font(.system(size: 16))
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 280, height: 72)
+                        .padding(.bottom, 50)
 
-                Circle()
-                    .frame(width: 264, height: 264)
-                    .foregroundColor(Color.gray100)
-
-                Text("스티키는 집에서만 이용할 수 있어요,\n주소 정보는 소중하게 지켜진답니다.".localized)
-                    .frame(width: 312, height: 48)
-                    .padding(.top)
-                    .padding(.bottom, 40)
-
-                Indicator(currentIndex: 0)
-                    .padding(.bottom, 56)
-
-                NavigationLink(destination: SearchAddress()) {
-                    GradientRoundedButton(
-                        content: "시작하기".localized,
-                        startColor: Color.gradientStart,
-                        endColor: Color.gradientEnd,
-                        width: 312,
-                        height: 48
-                    )
-                }
+                    NavigationLink(destination: SearchAddress()) {
+                        GradientRoundedButton(
+                            content: "위치정보 설정하기".localized,
+                            startColor: Color.black,
+                            endColor: Color.black,
+                            width: 202,
+                            height: 48
+                        )
+                    }
+                }.frame(width: 280, height: 290)
             }
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
