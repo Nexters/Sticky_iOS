9fd77f1b37614ee4e44921f66a7cb6c38c6e8536
kor-Randy <randy_95@naver.com>
2021-01-20 00:28:10 +0900

    feat(share): 공유 ui 구현

--- Sticky/Features/Share/Share.swift
+++ Sticky/Features/Share/Share.swift
@@ -8,13 +8,91 @@
 import SwiftUI
 
 struct Share: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.purple
+
+            VStack {
+                HStack(spacing: 16) {
+                    Button("현재 기록") {
+                        print("현재 기록")
+                    }.font(.title3)
+
+                    Button("누적 기록") {
+                        print("누적 기록")
+                    }.font(.title3)
+
+                    Button("최근 기록") {
+                        print("최근 기록")
+                    }.font(.title3)
+                }.padding(.bottom, 36)
+
+                CardSlide()
+
+                Text("나의 현재기록을 공유합니다")
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .padding(.top, 20)
+
+                HStack {
+                    Rectangle()
+                        .overlay(
+                            Button(action: {
+                                // 로컬 공유하기
+                            }) {
+                                Image(systemName: "square.and.arrow.up")
+                                    .frame(width: 48, height: 48)
+                                    .foregroundColor(.white)
+                            }
+                        )
+                        .frame(width: 48, height: 48)
+                        .cornerRadius(10)
+                        .foregroundColor(Color("lightPurple"))
+
+                    Spacer()
+                        .frame(maxWidth: 60)
+
+                    Button(action: {
+                        // 인스타 스토리 공유
+                    }){
+                        Image("instagram")
+                            .frame(width: 48, height: 48)
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 48, height: 48)
+                    .cornerRadius(10)
+                }
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backButton, trailing: downloadButton)
+    }
+
+    var backButton: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            Image("left-arrow")
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.white)
+        }
+    }
+
+    var downloadButton: some View {
+        Button(action: {
+            // 다운로드
+        }) {
+            Image(systemName: "download")
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.white)
+        }
     }
 }
 
 struct Share_Previews: PreviewProvider {
     static var previews: some View {
-        Preview(source: Share())
+        Share()
+            .environmentObject(UIStateModel())
     }
 }
