35de5566463b8ce3a5fb58ee047e2f90336c9029
Kim Mincheol <tree.ji@kakaocorp.com>
2021-01-25 23:14:01 +0900

    feat(timer): 타이머 ui 구현

---
+++ Sticky/Features/Timer/Views/TimerNotRunning.swift
@@ -0,0 +1,65 @@
+//
+//  TimerOn.swift
+//  Sticky
+//
+//  Created by 지현우 on 2021/01/25.
+//
+
+import SwiftUI
+
+struct TimerNotRunning: View {
+    @Binding var isTimerOn: Timer.TimerType
+    var body: some View {
+        VStack {
+//                NavigationLink(destination: MyPage()) {
+//                    Text("MyPage")
+//                }
+
+//                Button(action: {
+//                    popupState.isPresented = true
+//                }, label: {
+//                    Text("팝업")
+//                })
+            Spacer()
+
+            VStack {
+                Text("0일")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+
+                Text("00:00")
+                    .font(.system(size: 80))
+                    .bold()
+                    .foregroundColor(.white)
+
+                Text("00")
+                    .font(.system(size: 30))
+                    .foregroundColor(.white)
+            }
+            .padding()
+            Spacer()
+
+            Button(action: {
+                self.isTimerOn = .running
+            }, label: {
+                Circle()
+                    .frame(width: 92, height: 92)
+                    .foregroundColor(.white)
+                    .shadow(color: .black, radius: 10, x: 2, y: 2)
+                    .overlay(
+                        Image(systemName: "play.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(.leading, 10)
+                    )
+                    .padding(.bottom, 170)
+            })
+        }
+    }
+}
+
+struct TimerNotRunning_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerNotRunning(isTimerOn: .constant(Timer.TimerType.notRunning))
+    }
+}
---
+++ Sticky/Features/Timer/Views/TimerRunning.swift
@@ -0,0 +1,82 @@
+//
+//  TimerRunning.swift
+//  Sticky
+//
+//  Created by 지현우 on 2021/01/25.
+//
+
+import SwiftUI
+
+struct TimerRunning: View {
+    @EnvironmentObject private var popupState: PopupStateModel
+    @Binding var isTimerOn: Timer.TimerType
+    @Binding var sharePresented: Bool
+    var body: some View {
+        VStack {
+            Spacer()
+
+            VStack {
+                Text("0일")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+
+                Text("00:00")
+                    .font(.system(size: 80))
+                    .bold()
+                    .foregroundColor(.white)
+
+                Text("00")
+                    .font(.system(size: 30))
+                    .foregroundColor(.white)
+            }
+            .padding()
+            Spacer()
+
+            HStack {
+                Button(action: {
+                    self.isTimerOn = .stop
+                }, label: {
+                    Circle()
+                        .frame(width: 92, height: 92)
+                        .foregroundColor(.white)
+                        .shadow(color: .black, radius: 10, x: 2, y: 2)
+                        .overlay(
+                            Image(systemName: "pause.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.black)
+                        )
+                        .padding(.bottom, 170)
+                        .padding(.trailing,20)
+                })
+                
+
+                Button(action: {
+                    self.popupState.isPresented = true
+                }, label: {
+                    Circle()
+                        .frame(width: 92, height: 92)
+                        .foregroundColor(.black)
+                        .shadow(color: .black, radius: 10, x: 2, y: 2)
+                        .overlay(
+                            Image(systemName: "square.and.arrow.up")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.white)
+                        )
+                        .padding(.bottom, 170)
+                        .padding(.leading,20)
+                })
+                
+                
+            }
+        }
+    }
+}
+
+struct TimerRunning_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerRunning(isTimerOn: .constant(Timer.TimerType.running), sharePresented: .constant(true))
+            .environmentObject(PopupStateModel())
+    }
+}
