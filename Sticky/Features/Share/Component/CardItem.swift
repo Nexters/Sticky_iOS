34443ef0774f7dd36ed4b0d863cd53a0f880cc1d
Kim Mincheol <nexters@kakao.com>
2021-01-31 23:26:00 +0900

    fix(share): 2차 디자인 화면 적용

--- Sticky/Features/Share/Component/CardItem.swift
+++ Sticky/Features/Share/Component/CardItem.swift
@@ -7,13 +7,11 @@
 
 import SwiftUI
 
+// MARK: - CardItem
+
 // 각 카드 정보가 담기는 UI 뷰
 struct CardItem<Content: View>: View {
-    @EnvironmentObject var UIState: UIStateModel
-    let cardWidth: CGFloat
-    let cardHeight: CGFloat
-    var id: Int
-    var content: Content
+    // MARK: Lifecycle
 
     @inlinable public init(
         id: Int,
@@ -28,13 +26,27 @@ struct CardItem<Content: View>: View {
         self.id = id
     }
 
+    // MARK: Internal
+
+    @EnvironmentObject var UIState: UIStateModel
+    let cardWidth: CGFloat
+    let cardHeight: CGFloat
+    var id: Int
+    var content: Content
+
     var body: some View {
         // Active 카드인지 확인
         // Active 카드가 아닌 카드들은 여기서 UI/Design 수정
         content
-            .frame(width: cardWidth, height: id == UIState.activeCard ? cardHeight : cardHeight - 60, alignment: .center)
+            .frame(
+                width: cardWidth,
+                height: id == UIState.activeCard ? cardHeight : cardHeight - 60,
+                alignment: .center
+            )
     }
 }
+
+// MARK: - CardItem_Previews
 
 struct CardItem_Previews: PreviewProvider {
     static var previews: some View {
@@ -43,26 +55,30 @@ struct CardItem_Previews: PreviewProvider {
             Card(id: 0, level: 30, nickname: "이불밖은 위험해", totalTime: "10일 23시간 34분"),
             Card(id: 0, level: 30, nickname: "이불밖은 위험해", totalTime: "10일 23시간 34분"),
         ]
-        return ForEach(items, id: \.self.id) { item in
-            CardItem(
-                // 카드 UI의 id
-                id: Int(item.id),
-                // 카드 사이의 너비
-                spacing: 20,
-                // 가려진 카드의 너비
-                widthOfHiddenCards: 40,
-                // 카드의 높이
-                cardHeight: 360
-            ) {
-                Text("\(item.nickname)")
+        return ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(items, id: \.self.id) { item in
+                    CardItem(
+                        // 카드 UI의 id
+                        id: Int(item.id),
+                        // 카드 사이의 너비
+                        spacing: 20,
+                        // 가려진 카드의 너비
+                        widthOfHiddenCards: 40,
+                        // 카드의 높이
+                        cardHeight: 360
+                    ) {
+                        Text("\(item.nickname)")
+                    }
+                    .foregroundColor(Color.white)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .shadow(color: Color.gray, radius: 4, x: 0, y: 4)
+                    .transition(AnyTransition.slide)
+                    .animation(.spring())
+                    .environmentObject(UIStateModel())
+                }
             }
-            .foregroundColor(Color.white)
-            .background(Color.blue)
-            .cornerRadius(8)
-            .shadow(color: Color.gray, radius: 4, x: 0, y: 4)
-            .transition(AnyTransition.slide)
-            .animation(.spring())
-            .environmentObject(UIStateModel())
         }
     }
 }
