2b72b8e3dcf3630d16e971cb69677bae15c62eba
kor-Randy <randy_95@naver.com>
2021-02-08 14:52:51 +0900

    chore: 챌린지 시작 시, 시간 초기화

--- Sticky/Features/Timer/Components/BottomTimerNotRunning.swift
+++ Sticky/Features/Timer/Components/BottomTimerNotRunning.swift
@@ -25,6 +25,7 @@ struct BottomTimerNotRunning: View {
 //                })
 
             Button(action: {
+                self.challengeState.timeData = TimeData()
                 self.challengeState.numberOfHeart = 3
                 self.challengeState.type = .running
             }) {
