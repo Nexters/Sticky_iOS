579f9b05f9132134f92f0c43bbadda340715b44b
kor-Randy <randy_95@naver.com>
2021-02-08 23:30:19 +0900

    chore: 카드 뷰 색 수정

--- Sticky/Extensions/Notification.Name+.swift
+++ Sticky/Extensions/Notification.Name+.swift
@@ -10,4 +10,8 @@ import Foundation
 extension Notification.Name {
     static let enterGeofence = Notification.Name("enterGeofence")
     static let exitGeofence = Notification.Name("exitGeofence")
+    
+    static let captureScreen = Notification.Name("captureScreen")
+    static let shareLocal = Notification.Name("shareLocal")
+    static let shareInstagram = Notification.Name("shareInstagram")
 }
