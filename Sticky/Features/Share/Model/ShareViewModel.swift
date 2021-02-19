24273a09b9ea6abf78ff0cdab142b9553d793dda
Kim Mincheol <nexters@kakao.com>
2021-02-19 21:22:23 +0900

    fix(mypage-badge): 마이페이지 배지 선택 시 이미지 버그 수정

--- Sticky/Features/Share/Model/ShareViewModel.swift
+++ Sticky/Features/Share/Model/ShareViewModel.swift
@@ -13,17 +13,6 @@ class ShareViewModel: ObservableObject {
     init() {
         badge = loadBadge(forKey: "shareBadge")
         seconds = UserDefaults.standard.integer(forKey: "shareSeconds")
-//        let type = Main.ChallengeType(
-//            rawValue: UserDefaults.standard.integer(forKey: "challengeType")
-//        ) ?? .notRunning
-//        switch type {
-//        case .notAtHome,
-//             .notRunning,
-//             .outing:
-//            seconds = 0
-//        case .running:
-//            seconds = UserDefaults.standard.integer(forKey: "shareSeconds")
-//        }
     }
 
     // MARK: Internal
